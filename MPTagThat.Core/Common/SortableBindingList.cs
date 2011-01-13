389544792cd9271c9093d5ef3606b0b1c8f8f8d0
hwahrmann <helmut.wahrmann@gmx.at>
2011-01-13 14:15:53 +0000

    General: Implemented sorting of Grid on Multiple columns

--- MPTagThat.Core/Common/SortableBindingList.cs
+++ MPTagThat.Core/Common/SortableBindingList.cs
@@ -27,11 +27,90 @@ using System.ComponentModel;
 
 namespace MPTagThat.Core
 {
-  public class SortableBindingList<T> : BindingList<T>
+  public class SortableBindingList<T> : BindingList<T>, IBindingListView
   {
     private bool _isSorted;
     private ListSortDirection _sortDirection;
     private PropertyDescriptor _sortProperty;
+    private ListSortDescriptionCollection _sortDescriptions = new ListSortDescriptionCollection();
+    private List<PropertyComparer<T>> _comparers;
+
+    #region IBindingListView Members
+    public string Filter
+    {
+      get
+      {
+        throw new System.NotImplementedException();
+      }
+      set
+      {
+        throw new System.NotImplementedException();
+      }
+    }
+
+    public void RemoveFilter()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public ListSortDescriptionCollection SortDescriptions
+    {
+      get { return _sortDescriptions; }
+    }
+
+    public bool SupportsAdvancedSorting
+    {
+      get { return true; }
+    }
+
+    public bool SupportsFiltering
+    {
+      get { return false; }
+    }
+
+    public void ApplySort(ListSortDescriptionCollection sorts)
+    {
+      // Get list to sort
+      // Note: this.Items is a non-sortable ICollection<T>
+      List<T> items = this.Items as List<T>;
+
+      // Apply and set the sort, if items to sort
+      if (items != null)
+      {
+        _sortDescriptions = sorts;
+        _comparers = new List<PropertyComparer<T>>();
+        foreach (ListSortDescription sort in sorts)
+          _comparers.Add(new PropertyComparer<T>(sort.PropertyDescriptor, sort.SortDirection));
+        items.Sort(CompareValuesByProperties);
+      }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private int CompareValuesByProperties(T x, T y)
+    {
+      if (x == null)
+      {
+        return (y == null) ? 0 : -1;
+      }
+
+      if (y == null)
+      {
+        return 1;
+      }
+        
+      foreach (PropertyComparer<T> comparer in _comparers)
+      {
+        int retval = comparer.Compare(x, y);
+        if (retval != 0)
+        return retval;
+      }
+      return 0;
+    }
+
+    #endregion
 
     protected override bool SupportsSortingCore
     {
@@ -64,8 +143,12 @@ namespace MPTagThat.Core
       // Apply and set the sort, if items to sort
       if (items != null)
       {
-        PropertyComparer<T> pc = new PropertyComparer<T>(property, direction);
-        items.Sort(pc);
+        ListSortDescription sort = new ListSortDescription(property, direction);
+        ListSortDescription[] sortArr = new ListSortDescription[_sortDescriptions.Count + 1];
+        _sortDescriptions.CopyTo(sortArr, 1);
+        sortArr[0] = sort;
+        _sortDescriptions = new ListSortDescriptionCollection(sortArr);
+        ApplySort(_sortDescriptions);
         _isSorted = true;
       }
       else
