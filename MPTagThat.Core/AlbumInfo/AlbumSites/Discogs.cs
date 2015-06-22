259e3aa768d56c90c75e0b6e6acf2bb9e407188b
hwahrmann <hwahrmann@team-mediaportal.com>
2015-06-22 19:00:13 +0200

    removed unused usings

--- MPTagThat.Core/AlbumInfo/AlbumSites/Discogs.cs
+++ MPTagThat.Core/AlbumInfo/AlbumSites/Discogs.cs
@@ -18,15 +18,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading;
-using System.Web;
-using System.Xml;
 using DiscogsNet.Api;
-using DiscogsNet.Model;
 using DiscogsNet.Model.Search;
 
 namespace MPTagThat.Core.AlbumInfo.AlbumSites
