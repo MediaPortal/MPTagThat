9b1aba2705163cebce6f5741891830cb90b8a214
Helmut Wahrmann <hwahrmann@team-mediaportal.com>
2017-08-31 10:26:43 +0200

    Added Release Information

--- MPTagThat.Core/Properties/AssemblyInfo.cs
+++ MPTagThat.Core/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.1.7")]
-[assembly: AssemblyFileVersion("3.1.7")]
+[assembly: AssemblyVersion("3.1.8")]
+[assembly: AssemblyFileVersion("3.1.8")]
--- MPTagThat/Properties/AssemblyInfo.cs
+++ MPTagThat/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using Elegant.Ui;
 //      Revision
 //
 
-[assembly: AssemblyVersion("3.1.7")]
+[assembly: AssemblyVersion("3.1.8")]
 [assembly: DefaultTheme(EmbeddedTheme.Office2007Blue)]
